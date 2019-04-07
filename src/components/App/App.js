62ebfc0a67ff3d63dda7bf7ee31e315e0be7d0b4
nilyukov <nilyukov@gmail.com>
2019-04-07 14:23:01 +0300

    Init data to people list and get id person by click

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Header from '../Header/Header';
 import RandomPlanet from '../RandomPlanet/RandomPlanet';
@@ -7,22 +7,33 @@ import PersonDetails from '../PersonDetails/PersonDetails';
 
 import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
-        </div>
-      </div>
-    </div>
-  );
-};
+    state = {
+        selectedPerson: 0
+    }
 
-export default App;
+    onPersonSelected = (id) => {
+        this.setState({
+            selectedPerson: id
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <RandomPlanet/>
+
+                <div className="row mb2">
+                    <div className="col-md-6">
+                        <ItemList onItemSelected={this.onPersonSelected}/>
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails personId={this.state.selectedPerson}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
